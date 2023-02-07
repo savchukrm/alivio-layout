b9c3626fcd7af7815964b2505c52706f832949e1
Rymma Savchuk <savchukrmm@gmail.com>
2023-02-07 17:03:54 +0200

    Adaptive layout was made

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/* ================= MOBILE NAV ========================= */
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function () {
+  mobileNavIcon.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
+
 /* ================= VIDEO ========================= */
 const videoBtn = document.querySelector('#video__story-btn');
 const videoBtnIcon = document.querySelector('#video__story-btn-icon');
